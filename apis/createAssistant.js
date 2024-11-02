341e7dbb8163948b62745354d24f746e87f46c1e
Tu Nombre <manuelcalso@192.168.1.7>
2024-11-02 14:44:06 -0600

    reagreagar carp apis

---
+++ apis/createAssistant.js
@@ -0,0 +1,19 @@
+import OpenAI from "openai";
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+async function main() {
+  try {
+    const assistant = await openai.assistants.create({
+      name: "Math Tutor",
+      instructions:
+        "You are a personal math tutor. Write and run code to answer math questions.",
+      tools: [{ type: "code_interpreter" }],
+      model: "gpt-4",
+    });
+    console.log("Assistant created successfully:", assistant);
+  } catch (error) {
+    console.error("Error creating assistant:", error);
+  }
+}
+
+main();
---
+++ apis/fetchWeatherData.js
@@ -0,0 +1,52 @@
+export const fetchWeatherData = async () => {
+  try {
+    const apiKey = "612cbf06bf06319c9a453dfb40ec6f8e";
+
+    // Obtener la ubicación del usuario
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const weatherContainer = document.querySelector(".weather");
+        const icon = data.weather[0].icon; // Icono del clima
+        const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        const temperature = data.main.temp;
+        
+        // Cambiar el color de fondo basado en la temperatura
+        if (temperature <= 14) {
+          weatherContainer.style.backgroundColor = "blue"; // Azul para ≤ 15
+        } else if (temperature > 15 && temperature < 20) {
+          weatherContainer.style.backgroundColor = "orange"; // Naranja para > 15 y < 20
+        } else if (temperature >= 20 && temperature <= 25) {
+          weatherContainer.style.backgroundColor = "red"; // Rojo para ≥ 20 y ≤ 25
+        } else {
+          weatherContainer.style.backgroundColor = "red"; // Rojo para > 25
+        }
+
+        // Mostrar el contenido del clima, solo si se encuentra el icono
+        let iconHtml = "";
+        if (icon) {
+          iconHtml = `<img src="${iconUrl}" alt="Weather Icon">`;
+        }
+
+        weatherContainer.innerHTML = `<div class="weather-section">
+          <h1>Clima en ${data.name}</h1>
+          ${iconHtml}
+          <p>${data.main.temp} °C</p>
+          
+        </div>`;
+      },
+      (error) => {
+        console.error("Error al obtener la ubicación:", error);
+      }
+    );
+  } catch (error) {
+    console.error("Error al obtener el clima:", error);
+  }
+};
