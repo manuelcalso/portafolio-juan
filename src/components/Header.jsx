d8b4efcfa1e74f5c21ea6470c7056331439201a9
Tu Nombre <manuelcalso@192.168.1.7>
2024-11-16 12:13:09 -0600

    tetris icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ import Tetris from "../components/tetris/Tetris";
 
 const navigation = [
   { name: "Blog 'El Escritorio'", href: "/blog" },
-  { name: "Tetris", href: "/gran-clasico-juego-tetris-online" },
+  { name: "Tetris Game", href: "/gran-clasico-juego-tetris-online" },
   { name: "PlasticBeach", href: "https://plasticbeach.shop/" },
-  { name: "English version", href: "https://juancalleros.vercel.app/" },
+  { name: "English", href: "https://juancalleros.vercel.app/" },
 ];
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -36,16 +36,13 @@ export default function Header() {
                 target="_blank"
                 className="text-2xl slide01 float p-2 rounded-full hover:bg-green-800 hover:text-white hover:text-3xl hover:tracking-widest"
               >
-                {item.name === "English version" ? (
-                  <p className=" texto-animado">
-                    <span>E</span>
-                    <span>n</span>
-                    <span>g</span>
-                    <span>l</span>
-                    <span>i</span>
-                    <span>s</span>
-                    <span>h</span>{" "}
-                  </p>
+                {item.name === "Tetris Game" ? (
+                  <div className=" texto-animado flex flex-col justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-10 0 44 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z" />
+                    </svg>
+                    <p>T<span> etris</span></p>
+                  </div>
                 ) : (
                   item.name
                 )}
