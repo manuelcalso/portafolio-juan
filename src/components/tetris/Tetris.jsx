b04a28f4e825498864e2efd3f636279ba1ca9660
Tu Nombre <manuelcalso@192.168.1.8>
2025-01-25 17:44:30 -0600

    mejoras en tetris

--- src/components/tetris/Tetris.jsx
+++ src/components/tetris/Tetris.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Header from "../Header";
 
+
 const IframeComponent = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col justify-center items-center bg-gradient-to-t from-blue-500 to-black animate-bg-pan-top font-kalnia pt-10">
-        <h1 className="text-3xl font-bold mb-4 text-white">
-          Juega Tetris: El clásico de siempre
-        </h1>
-        <p className="text-lg text-gray-300 mb-4">
-          Disfruta de una versión de Tetris desarrollada con React. Desafía tus
-          habilidades y compite por la puntuación más alta. Da click y usa WASD
-          de tu teclado.
-        </p>
+      <div className="flex flex-col justify-center items-center bg-gradient-to-t from-orange-500 to-black animate-bg-pan-top  p-10">
+        <div className="w-98 bg-transparent flex flex-col justify-center items-center bg-gradient-to-t from-red-500 to-black animate-bg-pan-top ">
+          <h1 className="text-3xl font-bold mb-4 text-white">
+            Juega Tetris: El clásico de siempre
+          </h1>
+          <p className="text-lg text-gray-300 mb-4">
+            Disfruta de una versión de Tetris desarrollada con React. Desafía tus
+            habilidades y compite por la puntuación más alta.
+            <br />
+            {" "}
+            <span style={{ color: "yellowgreen" }}>
+              Da click y usa WASD
+              de tu teclado.
+            </span>
+          </p>
+        
         <iframe
           src="https://manuelcalso.github.io/tetris-game.github.io/"
           width="480"
@@ -23,10 +31,11 @@ const IframeComponent = () => {
           loading="lazy"
           aria-label="Juego de Tetris"
         ></iframe>
-        <p className="text-gray-200 mt-4 text-center">
+        <p className="text-white mt-4 text-center">
           Un sencillo juego de Tetris que desarrollé con React. ¡Pruébalo y
           mejora tu puntuación!
         </p>
+        </div>
       </div>
     </>
   );
